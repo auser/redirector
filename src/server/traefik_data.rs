e7ef442de8479519defb95cf23fcd0a7481db902
Ari <me@ari.io>
2024-10-28 20:08:15 -0700

    Updates to traefik parsing

--- src/server/traefik_data.rs
+++ src/server/traefik_data.rs
@@ -21,38 +21,18 @@ impl TryFrom<&HeaderMap> for TraefikData {
 
     fn try_from(headers: &HeaderMap) -> Result<Self, Self::Error> {
         debug!("TraefikData received headers: {:?}", headers);
-        let forwarded_host: String = headers.parse_optional_header("x-forwarded-host").unwrap_or("http://redirect-backend".to_string());
-
-        let (service_addr, service_url) = determine_backend_from_host(&forwarded_host)?;
-
+        let request_host: String = headers
+            .parse_optional_header("x-forwarded-host")
+            .unwrap_or("http://redirect-backend".to_string());
 
         Ok(Self {
-            service_addr,
-            service_url,
-            request_host: forwarded_host,
+            service_addr: headers.parse_header("ServiceAddr")?,
+            service_url: headers.parse_header("ServiceURL")?,
+            request_host,
             origin_status: headers.parse_optional_header("OriginStatus"),
             location: headers.parse_optional_header("origin_Location"),
             request_path: headers.parse_optional_header("RequestPath"),
             request_scheme: headers.parse_optional_header("RequestScheme"),
         })
-    }
-}
-
-fn determine_backend_from_host(host: &str) -> Result<(String, String), Box<dyn std::error::Error + Send + Sync>> {
-    match host.split('.').next() {
-        Some("test") => Ok((
-            "redirect-backend:80".to_string(),
-            "http://redirect-backend".to_string()
-        )),
-        Some("test2") => Ok((
-            "redirect-backend:80".to_string(),
-            "http://redirect-backend".to_string()
-        )),
-        _ => {
-            Ok((
-                "www.herringbank.com".to_string(),
-                "https://www.herringbank.com".to_string()
-            ))
-        }
     }
 }
