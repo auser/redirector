04420fb7fa583416f085bc58cff4356c518ecda4
Ari <me@ari.io>
2024-11-01 21:37:29 +0000

    Updates with git versioning

--- build.rs
+++ build.rs
@@ -1,14 +1,14 @@
-use vergen_git2::{BuildBuilder, CargoBuilder, Emitter, SysinfoBuilder};
+use vergen_git2::{BuildBuilder, Emitter, Git2Builder, SysinfoBuilder};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let build = BuildBuilder::all_build()?;
-    let cargo = CargoBuilder::all_cargo()?;
     let si = SysinfoBuilder::all_sysinfo()?;
+    let git2 = Git2Builder::all_git()?;
 
     Emitter::default()
         .add_instructions(&build)?
-        .add_instructions(&cargo)?
         .add_instructions(&si)?
+        .add_instructions(&git2)?
         .emit()?;
 
     Ok(())
